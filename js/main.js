6482cc60cc7c04a60c6d62f7ed00cae30b2aa268
MetsukiSutemi <seishi.isshun@gmail.com>
2025-05-04 22:45:05 +1000

    123

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://127.0.0.1:8000'
+const API_BASE_URL = 'https://shupep.ru/api'
 
 document.addEventListener('DOMContentLoaded', function () {
 	// Мобильное меню для главной страницы
